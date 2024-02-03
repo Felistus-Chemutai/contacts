c96c0c017bff96c6db34b986d54cc48505bf647c
Felistus-Chemutai <chemunicole@gmail.com>
2024-02-03 11:00:29 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./error-page";
+import Contact,{loader as contactLoader,   action as contactAction,
+} from "./routes/contact";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+import EditContact, { action as editAction,} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    action: contactAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          /* the rest of the routes */
+        ],
+      },
+
+      { index: true, element: <Index /> },
+
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
